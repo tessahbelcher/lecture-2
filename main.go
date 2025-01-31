aae5663e71d9435eaac7559dc6ce4d068042317e
tessahbelcher <tessahwork@gmail.com>
2025-01-31 05:56:36 -0800

    Commit unfinished edits

---
+++ main.go
@@ -0,0 +1,15 @@
+/**********************
+Lecture 2 Code
+Author: Tessah Belcher
+Date Completed:
+Description:
+**********************/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("My name is Tessah")
+}
