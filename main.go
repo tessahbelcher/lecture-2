274a34982fd9f5f07b790c64059d2b5d5321f615
tessahbelcher <tessahwork@gmail.com>
2025-02-14 11:13:57 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 /**********************
-Lecture 2 Code
+
 Author: Tessah Belcher
 Date Completed:
 Description:
 **********************/
 
-package main
-
-import "fmt"
-
-func main() {
 package main
 
 import (
@@ -19,17 +14,36 @@ import (
 )
 
 func init() {
-    rand.Seed(int64(time.Now().Nanosecond()))
+    rand.Seed(time.Now().UnixNano())
+}
+
+type Colleague struct {
+    Name  string
+    Blame string
 }
 
 func main() {
-	
-    var randomNumber1 int = rand.Intn(10) // generates and stores random int ranging from 0 to 9
-    var randomNumber2 int = rand.Intn(20) // generates and stores random int ranging from 0 to 19
-    var randomNumber3 int = rand.Intn(31) // generates and stores random int ranging from 0 to 30
+    var colleague Colleague
 
-    fmt.Println("Random Number 1:", randomNumber1)
-    fmt.Println("Random Number 2:", randomNumber2)
-    fmt.Println("Random Number 3:", randomNumber3)
+    fmt.Println("What is your colleague's first name?")
+    fmt.Scanln(&colleague.Name)
+
+    fmt.Println("Will you blame", colleague.Name, "for the software bug? (0 = no, 1 = yes)")
+
+    var response int
+    fmt.Scanln(&response)
+
 }
-}
+
+
+
+Note: [Colleague Name] is the name of the colleague typed in by the user at the beginning of the program.
+Quick Review: Program must have
+Colleague struct
+Name
+Blame
+User input for Name on instantiated Colleague variable (i.e., variable of type "Colleague")
+Random assignment of Blame for instantiated Colleague variable
+User input for blaming colleague or not
+Appropriate output based on potential outcome chart (above)
+
